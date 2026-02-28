ef61f336bd0689bb7d3379d49222d7f507200671
MD Motasim Billah <themotasimdev@gmail.com>
2026-02-28 23:32:22 +0600

    university details page ui done

--- src/components/layouts/blog/BlogDetails.jsx
+++ src/components/layouts/blog/BlogDetails.jsx
@@ -3,6 +3,7 @@ import { FaCalendarAlt } from "react-icons/fa";
 import blogs from "../../../data/blog";
 import PBase from "../../PBase";
 import { FaAngleRight } from "react-icons/fa6";
+import Container from "../../Container";
 
 const BlogDetails = () => {
   const { slug } = useParams();
@@ -13,65 +14,67 @@ const BlogDetails = () => {
 
   return (
     <section className="container mx-auto px-4 lg:pt-5 lg:pb-10">
-      {/* ======= bradcumb ======= */}
-      <div className="flex items-center">
-        <Link to={"/blogs"}>
-          <PBase text={"Blog"} className={"py-5"} />
-        </Link>
-        <FaAngleRight />
-        <PBase text={`${slug}`} className={'cursor-pointer'}/>
-      </div>
-      {/* ======= bradcumb ======= */}
-
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
-        {/* Left Blog Content */}
-        <div className="lg:col-span-8">
-          <img
-            src={blog.image}
-            alt={blog.title}
-            className="mb-6 h-80 w-full rounded-xl object-cover lg:hover:scale-103 transition-transform duration-500"
-          />
-
-          <h1 className="mb-3 text-3xl font-bold text-gray-800">
-            {blog.title}
-          </h1>
-
-          <div className="mb-6 flex items-center gap-2 text-sm text-gray-500">
-            <FaCalendarAlt size={14} />
-            <span>{blog.date}</span>
-          </div>
-
-          <p className="text-gray-700 leading-relaxed whitespace-pre-line">
-            {blog.content}
-          </p>
+      <Container>
+        {/* ======= bradcumb ======= */}
+        <div className="flex items-center">
+          <Link to={"/blogs"}>
+            <PBase text={"Blog"} className={"py-5"} />
+          </Link>
+          <FaAngleRight />
+          <PBase text={`${slug}`} className={"cursor-pointer"} />
         </div>
+        {/* ======= bradcumb ======= */}
 
-        {/* Right Sidebar */}
-        <aside className="lg:col-span-4">
-          <h3 className="mb-4 text-lg font-semibold text-gray-800">
-            Other Blogs
-          </h3>
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
+          {/* Left Blog Content */}
+          <div className="lg:col-span-8">
+            <img
+              src={blog.image}
+              alt={blog.title}
+              className="mb-6 h-80 w-full rounded-xl object-cover lg:hover:scale-103 transition-transform duration-500"
+            />
 
-          <div className="space-y-4">
-            {relatedBlogs.map((item) => (
-              <Link
-                key={item.id}
-                to={`/blog/${item.slug}`}
-                className="group flex gap-3 rounded-lg bg-white p-3 shadow-sm hover:shadow-md"
-              >
-                <img
-                  src={item.image}
-                  alt={item.title}
-                  className="h-16 w-20 rounded-md object-cover"
-                />
-                <p className="text-sm font-medium text-gray-700 group-hover:text-secondary">
-                  {item.title}
-                </p>
-              </Link>
-            ))}
+            <h1 className="mb-3 text-3xl font-bold text-gray-800">
+              {blog.title}
+            </h1>
+
+            <div className="mb-6 flex items-center gap-2 text-sm text-gray-500">
+              <FaCalendarAlt size={14} />
+              <span>{blog.date}</span>
+            </div>
+
+            <p className="text-gray-700 leading-relaxed whitespace-pre-line">
+              {blog.content}
+            </p>
           </div>
-        </aside>
-      </div>
+
+          {/* Right Sidebar */}
+          <aside className="lg:col-span-4">
+            <h3 className="mb-4 text-lg font-semibold text-gray-800">
+              Other Blogs
+            </h3>
+
+            <div className="space-y-4">
+              {relatedBlogs.map((item) => (
+                <Link
+                  key={item.id}
+                  to={`/blog/${item.slug}`}
+                  className="group flex gap-3 rounded-lg bg-white p-3 shadow-sm hover:shadow-md"
+                >
+                  <img
+                    src={item.image}
+                    alt={item.title}
+                    className="h-16 w-20 rounded-md object-cover"
+                  />
+                  <p className="text-sm font-medium text-gray-700 group-hover:text-secondary">
+                    {item.title}
+                  </p>
+                </Link>
+              ))}
+            </div>
+          </aside>
+        </div>
+      </Container>
     </section>
   );
 };
--- src/components/layouts/university/universityDetails.jsx
+++ src/components/layouts/university/universityDetails.jsx
@@ -1,22 +1,66 @@
 import { useParams, Link } from "react-router";
-import { FaCalendarAlt } from "react-icons/fa";
 import PBase from "../../PBase";
-import { FaAngleRight } from "react-icons/fa6";
+import { FaAngleRight, FaLocationDot } from "react-icons/fa6";
+import intiLogo from "/src/assets/university-logos/INTI-40.png";
+import intiImage from "/src/assets/inti-university.webp";
+import Container from "../../Container";
+import Heading from "../../Heading";
+import P18 from "../../P18";
 
 const universityDetails = () => {
   return (
     <>
       <section className="container mx-auto px-4 lg:pt-5 lg:pb-10">
-        {/* ======= bradcumb ======= */}
-        <div className="flex items-center">
-          <Link to={"/blogs"}>
-            <PBase text={"Blog"} className={"py-5"} />
-          </Link>
-          <FaAngleRight />
-          <PBase text={`${"id"}`} className={"cursor-pointer"} />
-        </div>
-        {/* ======= bradcumb ======= */}
-        
+        <Container>
+          {/* ======= bradcumb ======= */}
+          <div className="flex items-center">
+            <Link to={"/blogs"}>
+              <PBase text={"Blog"} className={"py-5"} />
+            </Link>
+            <FaAngleRight />
+            <PBase text={`${"id"}`} className={"cursor-pointer"} />
+          </div>
+          {/* ======= bradcumb ======= */}
+
+          {/* ======================== */}
+          <div className="p-4 md:p-10 md:flex items-center md:gap-x-7 bg-linear-to-b from-secondary/80 to-secondary/10 rounded-tl-2xl rounded-tr-2xl">
+            <div className="py-6 md:py-13 w-40 md:w-55 mx-auto md:mx-0 bg-white">
+              <img src={intiLogo} alt={"intiLogo"} className="h-full w-full" />
+            </div>
+            <div className="mt-3 md:mt-0 text-center md:text-left flex flex-col gap-y-1 md:gap-y-3">
+              <Heading
+                text={"INTI University"}
+                className={"text-white font-bold"}
+              />
+              <div className="flex items-center mx-auto md:m-0">
+                <FaLocationDot className="text-white" />
+                <P18 text={"Location"} className={"text-white font-bold"} />
+              </div>
+            </div>
+          </div>
+          {/* ======================== */}
+
+          {/* ============================= */}
+          <div className="mt-10">
+            <h3 className="lg:text-2xl font-semibold">About the University</h3>
+            <P18
+              text={
+                "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos tempora eaque totam a esse? Voluptatum dolor fugit assumenda nulla, nam hic pariatur inventore quidem mollitia maxime excepturi? Unde, veniam ipsa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi incidunt quae voluptates, cumque recusandae eius eveniet modi velit amet nostrum unde non! Nesciunt pariatur explicabo ipsam veritatis nihil animi ut?"
+              }
+              className={"text-justify py-3 md:py-5"}
+            />
+            <div className="w-80 md:w-150 mx-auto">
+              <img src={intiImage} alt="" className="h-full w-full" />
+            </div>
+            <div className="py-2 pl-10 bg-secondary/80 mt-6">
+              <P18 text={"Course Name"} className={"text-white font-bold"} />
+            </div>
+            <div className="py-2 pl-10 bg-secondary/50 ">
+              <P18 text={"Course Name"} className={"text-white"} />
+            </div>
+          </div>
+          {/* ============================= */}
+        </Container>
       </section>
     </>
   );
