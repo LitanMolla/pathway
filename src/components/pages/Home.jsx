7554f43fe558917d41e0763ef3c8fd99fa89cc51
LitanMolla <litanmolla9@gmail.com>
2026-03-05 23:41:03 +0600

    loasder fixed

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,26 +3,9 @@ import About from "../layouts/home/About";
 import YourJuorney from "../layouts/home/YourJuorney";
 import OurPartnersU from "../layouts/home/OurPartnersU";
 import BecomeAPartner from "../layouts/home/BecomeAPartner";
-import Loader from "./Loader";
-import { useEffect, useState } from "react";
 
 const Home = () => {
-  const [showLoader, setShowLoader] = useState(false);
 
-  useEffect(() => {
-    // check if loader shown already
-    const loaderShown = sessionStorage.getItem("loaderShown");
-    if (!loaderShown) {
-      setShowLoader(true);
-      const timer = setTimeout(() => {
-        setShowLoader(false);
-        sessionStorage.setItem("loaderShown", "true");
-      }, 1000); // 1 sec
-      return () => clearTimeout(timer);
-    }
-  }, []);
-
-  if (showLoader) return <Loader />;
   return (
     <>
         <Banner />
